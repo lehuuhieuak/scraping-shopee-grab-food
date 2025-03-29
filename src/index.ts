33bfd723446a7ff75556c0508636cc8b2f6c691e
lehuuhieuak <01643815296h@gmail.com>
2025-03-29 22:45:35 +0700

    feat: add cors

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import dotenv from 'dotenv';
 import express, { Express } from 'express';
+import cors from 'cors';
 
 import { route } from './routes';
 
@@ -10,6 +11,8 @@ const PORT = process.env.PORT || 3000;
 
 // for parsing application/json
 app.use(express.json());
+
+app.use(cors());
 
 route(app);
 
